4d0061758d092561e6d7b5a2205f53298f2a6666
avidaldo <avd.formacion@gmail.com>
2022-02-14 22:51:00 +0100

    Ejemplo de delegación de clases

--- ej8scopeFunctions.kt
+++ ej8scopeFunctions.kt
@@ -9,7 +9,7 @@ import kotlin.random.Random
  *
  *
  * Las funciones de ámbito (scope) buscan simplemente mejorar la legibilidad del código
- * 
+ *
  * https://hackernoon.com/hn-images/1*bD7r_xcW5CWN389WEfgPDg.png
  * https://hackernoon.com/kotlin-a-deeper-look-8569d4da36f
  *
@@ -72,11 +72,12 @@ fun testScopeFunctions2() {
      * de modo que podrían seguirse haciendo referencias al propio objeto. */
 
     val mailer2 =
-        Mailer()
-            .apply { from("builder@agiledeveloper.com") }
-            .apply { to("venkats@agiledeveloper.com") }
-            .apply { subject("Your code sucks") }
-            .apply { body("details") }
+        Mailer().apply {
+                from("builder@agiledeveloper.com")
+                to("venkats@agiledeveloper.com")
+                subject("Your code sucks")
+                body("details")
+            }
     val result2 = mailer2.send()
     println("result2:\n$result2")
 
@@ -180,7 +181,6 @@ fun testScopeFunctions4let() {
 }
 
 
-
 fun testScopeFunctions4with() {
 
     /** with no es una función de extensión (a diferencia de las otras funciones de ámbito)
@@ -206,7 +206,7 @@ fun testScopeFunctions4with() {
 fun testScopeFunctions4also() {
     val numbers = mutableListOf("one", "two", "three")
 
-    numbers.let{ println("The list elements before adding new one: $it") }
+    numbers.let { println("The list elements before adding new one: $it") }
 
     /** also permite acceder al objeto contexto como let (con it), pero además devuelve el objeto.
      */
---
+++ ej9delegates.kt
@@ -0,0 +1,63 @@
+
+/***************************************************************************
+ ** Delegación de clases **********************************
+ **************************************************************************
+ *
+ * https://medium.com/androiddevelopers/delegating-delegates-to-kotlin-ee0a0b21c52b
+ *
+ */
+
+
+/** Caso de uso: queremos poder recuperar el último item eliminado de un ArrayList */
+
+
+/** Con herencia */
+class ListWithTrashInheritance<T> : ArrayList<T>() {
+    var deletedItem: T? =null
+
+    override fun remove(element: T): Boolean {
+        deletedItem = element
+        return super.remove(element)
+    }
+
+}
+
+/** Con delegación */
+class ListWithTrashDelegation <T>(
+    val innerList: MutableList<T> = ArrayList<T>()
+) : MutableCollection<T> by innerList {
+    var deletedItem : T? = null
+
+    override fun remove(element: T): Boolean {
+        deletedItem = element
+        return innerList.remove(element)
+    }
+}
+
+
+fun main(){
+    var lista1 = ListWithTrashInheritance<String>().apply {
+        addAll(arrayListOf("one","two", "three", "four"))
+    }
+    val lista2 = arrayOf("one","two", "three", "four").toCollection(ListWithTrashInheritance())
+
+    println(lista1)
+    println(lista2)
+
+    println(lista1.apply { remove("two")  }.deletedItem)
+    println(lista1.apply { remove("two") }.deletedItem)
+
+
+
+    var lista3 = ListWithTrashDelegation<String>().apply {
+        addAll(arrayListOf("one","two", "three", "four"))
+    }
+    val lista4 = arrayOf("one","two", "three", "four").toCollection(ListWithTrashDelegation())
+
+    println(lista3.innerList)
+    println(lista4.innerList)
+
+    println(lista3.apply { remove("two")  }.deletedItem)
+    println(lista4.apply { remove("two") }.deletedItem)
+
+}
